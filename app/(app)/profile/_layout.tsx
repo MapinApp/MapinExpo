aaea4eea65cd1a05d03bc66c4b5e901f6b70df0a
shavvimal <shavhugan@gmail.com>
2024-02-05 12:43:18 +0000

    status bar

--- app/(app)/profile/_layout.tsx
+++ app/(app)/profile/_layout.tsx
@@ -1,10 +1,12 @@
-import { Stack, Redirect } from "expo-router";
+import { Stack } from "expo-router";
 // Components
 import { Block } from "@/components/ui";
+import { StatusBar } from "expo-status-bar";
 
 export default function Layout() {
   return (
     <>
+      <StatusBar style="light" backgroundColor="transparent" />
       {/* Add view so it doesn't flicker white between screens. */}
       <Block background flex={1} justify={"center"}>
         <Stack
